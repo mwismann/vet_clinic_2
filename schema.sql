45391aa1ba3c705228f6325a0d1cd93c8939f96f
Matt <mwbertolucci@gmail.com>
2023-08-21 16:32:37 -0400

    Insert data for performance audit

--- schema.sql
+++ schema.sql
@@ -64,3 +64,6 @@ CREATE TABLE visits (
   vet_id INT REFERENCES vets(id),
   date_of_visit DATE NOT NULL
 )
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
